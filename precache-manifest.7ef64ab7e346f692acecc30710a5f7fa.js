60009655a96e2be860cf4ebd21332157d052c2a8
Kevin Miller <kmillergit@outlook.com>
2020-10-07 19:30:14 -0700

    Updates

---
+++ precache-manifest.7ef64ab7e346f692acecc30710a5f7fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7ad8c8a99ef11a95edd000c0bdc6a94",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "5b267684cbc51f2cbab4",
+    "url": "/employee-directory/static/css/main.43b10a03.chunk.css"
+  },
+  {
+    "revision": "6d4f8d9cd6e7b5c1ac0d",
+    "url": "/employee-directory/static/js/2.3d1cdfe5.chunk.js"
+  },
+  {
+    "revision": "213867b99db01cbfd59e56dd6b11de0e",
+    "url": "/employee-directory/static/js/2.3d1cdfe5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b267684cbc51f2cbab4",
+    "url": "/employee-directory/static/js/main.7781aa6f.chunk.js"
+  },
+  {
+    "revision": "79d7c4f4ad2a4d373d4e",
+    "url": "/employee-directory/static/js/runtime-main.759497ab.js"
+  },
+  {
+    "revision": "a6d4e45551929c1ed90870b147329455",
+    "url": "/employee-directory/static/media/LogoMakr_4SDhef.a6d4e455.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4872f4127f42b216424c250403435d3f.js"
+  "/employee-directory/precache-manifest.7ef64ab7e346f692acecc30710a5f7fa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/employee-directory/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
