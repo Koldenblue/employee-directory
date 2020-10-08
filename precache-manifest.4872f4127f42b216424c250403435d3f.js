e31902db44ca88ef113ebf5ceec82aef8b6fbdf5
Kevin Miller <kmillergit@outlook.com>
2020-10-07 19:08:12 -0700

    Updates

---
+++ precache-manifest.4872f4127f42b216424c250403435d3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "850025e3546566df2356d7b3def2c3fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b267684cbc51f2cbab4",
+    "url": "/static/css/main.43b10a03.chunk.css"
+  },
+  {
+    "revision": "2a45b502698f18dd7c58",
+    "url": "/static/js/2.5338cc64.chunk.js"
+  },
+  {
+    "revision": "213867b99db01cbfd59e56dd6b11de0e",
+    "url": "/static/js/2.5338cc64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b267684cbc51f2cbab4",
+    "url": "/static/js/main.7781aa6f.chunk.js"
+  },
+  {
+    "revision": "c74b6dcb725f329d7bfe",
+    "url": "/static/js/runtime-main.403e0656.js"
+  },
+  {
+    "revision": "a6d4e45551929c1ed90870b147329455",
+    "url": "/static/media/LogoMakr_4SDhef.a6d4e455.png"
+  }
+]);
